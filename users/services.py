123b457b46766d2f355ccdfc7f635765a2160c31
Azqii <109161021+Azqii@users.noreply.github.com>
2023-03-05 01:57:45 +0700

    minor transaction update

--- users/services.py
+++ users/services.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db import transaction
 
 from .repository import UserRepository, ProfileRepository
 
@@ -7,16 +8,17 @@ User = get_user_model()
 
 def save_user_profile_info(user: User, info: dict) -> None:
     """Сохраняет информацию о юзере и его профиле."""
-    UserRepository.update_name(user=user, first_name=info["first_name"], last_name=info["last_name"])
-    ProfileRepository.update_info(
-        profile=user.profile, photo=info["photo"], vk=info["vk"],
-        tg=info["tg"], instagram=info["instagram"], bio=info["bio"]
-    )
+    with transaction.atomic():
+        UserRepository.update_name(user=user, first_name=info["first_name"], last_name=info["last_name"])
+        ProfileRepository.update_info(
+            profile=user.profile, photo=info["photo"], vk=info["vk"],
+            tg=info["tg"], instagram=info["instagram"], bio=info["bio"]
+        )
 
 
 def get_user_info(user: User) -> dict:
     """Возвращает информацию о пользователе user, в которую входит его имя, описание, фото и профили в соц. сетях"""
-    info =  {
+    info = {
         "first_name": user.first_name, "last_name": user.last_name,
         "vk": user.profile.vk, "tg": user.profile.tg,
         "instagram": user.profile.instagram, "bio": user.profile.bio,
